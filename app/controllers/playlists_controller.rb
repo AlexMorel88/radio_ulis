b1add65d3c0043b011ff6c9d34c69e545d1c7004
AlexMorel88 <alexandre.morel@axomove.com>
2019-11-18 21:11:05 +0100

    ok pour counter maison provisoire

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -3,6 +3,8 @@ class PlaylistsController < ApplicationController
 
 	def index
 		@playlists = Playlist.all	
+		@counter = Counter.first.number+1
+		Counter.first.update(number: Counter.first.number+1)
 	end
 
 	def archive
---
+++ db/migrate/20191118195223_create_counters.rb
@@ -0,0 +1,11 @@
+class CreateCounters < ActiveRecord::Migration[5.2]
+  def change
+    create_table :counters do |t|
+    	t.integer :number   
+
+      t.timestamps
+    end
+
+    
+  end
+end
